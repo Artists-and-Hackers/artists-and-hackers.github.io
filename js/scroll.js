799fa5339bf5a9ed0353b8fbc2f39b85182330d4
Caleb Stone <caleblstone@gmail.com>
2020-07-30 11:59:42 -0400

    starting over

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-let arrow = document.querySelector('section.hero section.arrow a')
-
-arrow.addEventListener("click", function(){
-  document.querySelector('section.socialmedia').scrollIntoView({
-  behavior: 'smooth'
-  });
-})
