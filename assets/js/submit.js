4ff8954edcfce3698b9affcda1c14b08c3941a18
Caleb Stone <caleblstone@gmail.com>
2020-08-24 14:16:23 -0400

    website finished w/out mobile responsive

---
+++ assets/js/submit.js
@@ -0,0 +1,9 @@
+let buttons = document.querySelectorAll("#submit")
+let signup = document.querySelector("section.signup")
+let back = document.querySelector("div.back")
+
+buttons.forEach((button) => {
+  button.addEventListener('click', event => {
+    document.querySelector("section.signup").style.display = "flex"
+  });
+});
