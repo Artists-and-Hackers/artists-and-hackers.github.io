7ae4737e888b3bb446854b610f9ef7f4cd780853
Caleb Stone <caleblstone@gmail.com>
2021-12-20 15:30:31 -0500

    Mobile responsivity, better tagging system, Rss on individual eps, meta tags

--- assets/js/changeOrder.js
+++ assets/js/changeOrder.js
@@ -17,139 +17,47 @@ order.addEventListener("change", function(){
   }
 })
 
-//for tags
+let tags = [
+  "Technological Criticality",
+  "Esolangs",
+  "Livecoding",
+  "Radical Technology",
+  "Activism",
+  "Tools",
+  "Algorithmic Inequality",
+  "Coding Languages",
+  "Computing History",
+  "Digital Commons",
+  "Alternative Networks",
+  "Worldbuilding",
+  "Speculative Futurism"
+]
+
+for (var i = 0; i < tags.length; i++) {
+  let option = document.createElement("option")
+  let textnode = document.createTextNode(tags[i])
+  option.appendChild(textnode)
+  option.value = tags[i]
+  subject.appendChild(option)
+}
+
 subject.addEventListener("change", function(){
   let selected = subject.value
   episode.forEach(tag => {
-
-    //All Subjects
+    for (let i = 0; i < tags.length; i++) {
+        if (selected == tags[i]){
+          let tagComputified = tags[i].toLowerCase().replace(" ", "_")
+          if (tag.classList.contains(tagComputified) == true){
+            tag.style.display = "flex"
+          }
+          if (tag.classList.contains(tagComputified) == false){
+            tag.style.display = "none"
+          }
+        }
+    }
+    
     if (selected == "") {
       tag.style.display = "flex"
-      console.log(tag.classList.contains('technological_criticality'));
     }
-
-    if (selected == "Esolangs") {
-      if (tag.classList.contains('esolangs')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('esolangs') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Livecoding") {
-      if (tag.classList.contains('livecoding')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('livecoding') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Technological Criticality") {
-      console.log(tag.classList);
-      if (tag.classList.contains('technological_criticality')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('technological_criticality') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Radical Technology") {
-      if (tag.classList.contains('radical_technology')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('radical_technology') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Activism") {
-      if (tag.classList.contains('activism')) {
-        tag.style.display = "grid"
-      }
-      if (tag.classList.contains('activism') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Tools") {
-      if (tag.classList.contains('tools')) {
-        tag.style.display = "grid"
-      }
-      if (tag.classList.contains('tools') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Algorithmic Inequality") {
-      if (tag.classList.contains('algorithmic_inequality')) {
-        tag.style.display = "grid"
-      }
-      if (tag.classList.contains('algorithmic_inequality') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Coding Languages") {
-      if (tag.classList.contains('coding_languages')) {
-        tag.style.display = "grid"
-      }
-      if (tag.classList.contains('coding_languages') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Computing History") {
-      if (tag.classList.contains('computing_history')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('computing_history') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Digital Commons") {
-      if (tag.classList.contains('digital_commons')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('digital_commons') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Alternative Networks") {
-      if (tag.classList.contains('alternative_networks')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('alternative_networks') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Worldbuilding") {
-      if (tag.classList.contains('worldbuilding')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('worldbuilding') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-    if (selected == "Speculative Futurism") {
-      if (tag.classList.contains('speculative_futurism')) {
-        tag.style.display = "flex"
-      }
-      if (tag.classList.contains('speculative_futurism') == false) {
-        tag.style.display = "none"
-      }
-    }
-
-
-
-
-
   });
-
 })
