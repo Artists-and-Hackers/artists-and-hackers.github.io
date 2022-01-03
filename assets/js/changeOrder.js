f5bbc799a0fb8d2645e6785f4273b09b04b80745
Caleb Stone <caleblstone@gmail.com>
2022-01-03 13:00:46 -0500

    tools section up and running

--- assets/js/changeOrder.js
+++ assets/js/changeOrder.js
@@ -5,6 +5,8 @@ let order = document.querySelector("select.order")
 //for tags
 let subject = document.querySelector("select.subject")
 let episode = document.querySelectorAll("section.episode")
+let tool = document.querySelectorAll("section.tool")
+
 
 //for chronology
 order.addEventListener("change", function(){
@@ -16,6 +18,9 @@ order.addEventListener("change", function(){
     list.style.flexDirection = "column"
   }
 })
+
+
+
 
 let tags = [
   "Technological Criticality",
@@ -39,7 +44,11 @@ for (var i = 0; i < tags.length; i++) {
   option.appendChild(textnode)
   option.value = tags[i]
   subject.appendChild(option)
+
 }
+
+
+
 
 subject.addEventListener("change", function(){
   let selected = subject.value
@@ -55,7 +64,25 @@ subject.addEventListener("change", function(){
           }
         }
     }
-    
+
+    if (selected == "") {
+      tag.style.display = "flex"
+    }
+  });
+
+  tool.forEach(tag => {
+    for (let i = 0; i < tags.length; i++) {
+        if (selected == tags[i]){
+          let tagComputified = tags[i].toLowerCase().replace(" ", "_")
+          if (tag.classList.contains(tagComputified) == true){
+            tag.style.display = "flex"
+          }
+          if (tag.classList.contains(tagComputified) == false){
+            tag.style.display = "none"
+          }
+        }
+    }
+
     if (selected == "") {
       tag.style.display = "flex"
     }
--- tools/_posts/2020-08-06-Esoteric Codes.markdown
+++ tools/_posts/2020-08-06-Esoteric Codes.markdown
@@ -1,11 +1,15 @@
 ---
 layout: tool
-related-episode: Ep. 2
+related-episode: "Ep. 2"
+epLink: "https://link_to_episode_2.com/"
 title:  "Esoteric Codes"
-creator: Daniel Tempkin
+creators: [Daniel Tempkin]
+creatorLinks: ["https://danieltemkin.com/"]
 date:   2020-08-03 12:02:19 -0400
 categories: tools
+header-image: "/assets/images/tool_images/..."
 description: "Grayson Earle, Sam Lavigne, Francis Tseng, and Brian Clifton describe the tools they have created to subvert digital hegemony and aid in global protest"
+toolLink: "https://esoteric.codes/"
 tags: ["Technological Criticality"]
 code_tags: ["technological_criticality"]
 ---
